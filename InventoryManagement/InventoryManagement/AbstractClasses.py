c288a2591c837b7f8a94fc84f912102cc1f651e9
Kevin Brown <kevin.brown.cs@gmail.com>
2017-05-15 15:00:55 -0400

    Added more to the Abstract Classes and their overider and began to instantiate the Inventory Management

--- InventoryManagement/InventoryManagement/AbstractClasses.py
+++ InventoryManagement/InventoryManagement/AbstractClasses.py
@@ -2,6 +2,7 @@ import abc
 #
 class IInventory:
     __metaclass__ = abc.ABCMeta
+    
     OurInventory = dict()
     @abc.abstractmethod
     def hash(self):
@@ -27,3 +28,14 @@ class IItem:#Abstract class for items, all created items must be based on this c
     @abc.abstractmethod
     def hash(self):
         self.name
+class Inventory(IInventory):
+    OurInventory
+    def __init__(self):
+        self.OurInventory=dict()
+        return
+    
+    def hash(self):
+        super().hash()
+    def insert(self, item):
+        hold = {item.name,item}
+        self.OurInventory.update(hold)
--- InventoryManagement/InventoryManagement/main.py
+++ InventoryManagement/InventoryManagement/main.py
@@ -1,6 +1,18 @@
-
+from getpass import getpass
 
 #This fills in the data we need
 def initiateItemization():
+    UserAuth()
 
+    return
+def UserAuth():
+    username=raw_input("Please Enter Your Username\n")
+    password=getpass()
+    #TODO have it log into server and access that
+    ##Currently having the username and pass don't matter
+    #######
+    #login = LoginAuth()
+    #bac=login.loginHandler(username,password)
+    #if bac != "Failed" go through
+    #######
     return
